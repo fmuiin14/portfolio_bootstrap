14ca097be6f25a624840cf2ba081f0d40d646ee8
fmuiin14 <fmuiin14@gmail.com>
2018-11-20 05:51:15 +0700

    penambahan smooth scroll

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//event pada saat link
+$('.page-scroll').on('click', function(e) {
+    
+    //ambil isi href
+    var tujuan = $(this).attr('href');
+    //tangkap element yg bersangkutan
+    var elemenTujuan = $(tujuan);
+
+    
+    //pindahkan scroll
+    $('html,body').animate ({
+        scrollTop: elemenTujuan.offset().top - 50
+
+    }, 1250, 'easeInOutExpo');
+
+    e.preventDefault();
+
+    });
